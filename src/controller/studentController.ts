2254e18326587b4ad5769a7eec649118a7edf980
Dada Khalandar <kerry.blig12@gmail.com>
2021-12-28 22:46:36 +0530

    added Router

--- src/controller/studentController.ts
+++ src/controller/studentController.ts
@@ -1,26 +1,25 @@
-const express = require('express');
-const router = express.Router();
+const {Router} = require('express');
 const { Student: any } = require('../model/studentsModel')
 
 
-router.post('', async (req, res) => {
+Router.post('', async (req, res) => {
     const student = await Student.create(req.body);
     return res.status(201).send(student)
 })
 
-router.get('', async (req, res) => {
+Router.get('', async (req, res) => {
     const students = await Student.find().lean().exec();
     return res.status(200).send(students)
 });
 
-router.delete('/:id', async (req, res) => {
+Router.delete('/:id', async (req, res) => {
     const student = await Student.findByIdAndDelete(req.params.id);
     return res.status(200).send(student)
 
 })
 
 
-router.get('/:id', async (req, res) => {
+Router.get('/:id', async (req, res) => {
     try {
         const student = await Student.findById(req.params.id);
         return res.status(200).send(student)
@@ -31,4 +30,4 @@ router.get('/:id', async (req, res) => {
 })
 
 
-module.exports = router;
+module.exports = Router;
